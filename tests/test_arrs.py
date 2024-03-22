d6a255f3e4bc74759425f83bc4f9b058984a31d5
alextk37 <alextk37@yandex.ru>
2024-03-22 13:52:21 +0300

    add fixture for get array & ckllections

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,41 +4,49 @@ import pytest
 
 
 class TestArrs:
-    @pytest.mark.parametrize(
-            'array, ind, default, value',
-            [
-                ([1, 2, 3], 1, 'test', 2),
-                ([], 0, 'test', 'test'),
-                ([1, 2, 3], 4, 'test', 'test')
-            ]
-    )
-    def test_get(self, array, ind, default, value):
-        assert get(array, ind, default) == value
+    @pytest.fixture
+    def get_array(self):
+        return [1, 2, 3]
 
     @pytest.mark.parametrize(
-        'array, start_point, end_point, res_slice',
+            'ind, default, value',
+            [
+                (1, 'test', 2),
+                (4, 'test', 'test')
+            ]
+    )
+    def test_get(self, get_array, ind, default, value):
+        assert get(get_array, ind, default) == value
+        assert get([], 4, 'test') == 'test'
+
+    @pytest.mark.parametrize(
+        'start_point, end_point, res_slice',
         [
-            ([1, 2, 3, 4], 1, 3, [2, 3]),
-            ([1, 2, 3], 1, None, [2, 3]),
-            ([1, 2, 3, 4], None, 3, [1, 2, 3]),
-            ([1, 2, 3], None, None, [1, 2, 3]),
-            ([1, 2, 3], -4, None, [1, 2, 3]),
-            ([1, 2, 3], None, 6, [1, 2, 3]),
-            ([], None, 6, [])
+            (1, 3, [2, 3]),
+            (1, None, [2, 3]),
+            (None, 3, [1, 2, 3]),
+            (None, None, [1, 2, 3]),
+            (-4, None, [1, 2, 3]),
+            (None, 6, [1, 2, 3]),
         ]
     )
-    def test_my_slice(self, array, start_point, end_point, res_slice):
-        assert my_slice(array, start_point, end_point) == res_slice
+    def test_my_slice(self, get_array, start_point, end_point, res_slice):
+        assert my_slice(get_array, start_point, end_point) == res_slice
+        assert my_slice([], None) == []
 
 
 class TestDicts:
+    @pytest.fixture
+    def collections(self):
+        return {'a': 1, 'b': 2, 'c': 3}
+
     @pytest.mark.parametrize(
-        'collection, key, default, value',
+        'key, default, value',
         [
-            ({'a': 1, 'b': 2, 'c': 3}, 'c', 'git', 3),
-            ({'a': 1, 'b': 2, 'c': 3}, None, 'git', 'git'),
-            ({'a': 1, 'b': 2, 'c': 3}, 'd', 'git', 'git')
+            ('c', 'git', 3),
+            (None, 'git', 'git'),
+            ('d', 'git', 'git')
         ]
     )
-    def test_get_val(self, collection, key, default, value):
-        assert get_val(collection, key, default) == value
+    def test_get_val(self, collections, key, default, value):
+        assert get_val(collections, key, default) == value
